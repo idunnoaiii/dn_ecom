e1f7bffcdcc0152e5c8fcf4cd8776fc1d57c0f66
idunno.aiii@gmail.com <idunno.aiii@gmail.com>
2024-01-05 05:27:24 +0700

    [basket service] add get discount from discount server

--- backend/src/presentation/console/Program.cs
+++ backend/src/presentation/console/Program.cs
@@ -5,8 +5,9 @@ using Dumpify;
 BlockingCollection<string> queue = [];
 
 
-new Thread(() => {
-    foreach(var item in queue.GetConsumingEnumerable()) 
+new Thread(() =>
+{
+    foreach (var item in queue.GetConsumingEnumerable())
     {
         Console.WriteLine("print");
         Console.WriteLine(item);
@@ -14,7 +15,8 @@ new Thread(() => {
 }).Start();
 
 
-while (true) {
+while (true)
+{
     Console.WriteLine("please input");
     var input = Console.ReadLine();
     queue.Add(input);
@@ -27,24 +29,24 @@ while (true) {
 //     private readonly BlockingCollection<(string item, TaskCompletionSource<string> result)> _queue =
 //         new BlockingCollection<(string item, TaskCompletionSource<string> result)>();
 //     private readonly Task _processItemsTask;
- 
+
 //     public DatabaseFacade() => _processItemsTask = Task.Run(ProcessItems);
- 
+
 //     public void Dispose() => _queue.CompleteAdding();
- 
+
 //     public Task SaveAsync(string command)
 //     {
 //         var tcs = new TaskCompletionSource<string>();
 //         _queue.Add((item: command, result: tcs));
 //         return tcs.Task;
 //     }
- 
+
 //     private async Task ProcessItems()
 //     {
 //         foreach (var item in _queue.GetConsumingEnumerable())
 //         {
 //             Console.WriteLine($"DatabaseFacade: executing '{item.item}'...");
- 
+
 //             // Waiting a bit to emulate some IO-bound operation
 //             await Task.Delay(100);
 //             item.result.SetResult("OK");
